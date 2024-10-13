4e98021f770fe55347b7ebcaa20081d3426b4455
Jimmy Nguyen <114447986+MilkTeaAddict@users.noreply.github.com>
2024-10-13 00:21:33 -0700

    Add files via upload

---
+++ home-page.py
@@ -0,0 +1,45 @@
+import joblib
+import streamlit as st
+import pandas as pd
+from io import StringIO
+import numpy as np
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+
+import logistic
+from outliers import removeOutliers
+import svm
+
+st.header('ECS-171 Project - Classify Credit Score', divider='blue')
+
+logistic = joblib.load("models/logistic_no_outliers")
+svmLinear = joblib.load("models/svc_linear_no_outliers")
+svmRBF = joblib.load("models/svc_rbf_no_outliers")
+
+delay_from_due_date = st.slider("Delay from Due Date", 0, 50, 1)
+num_of_delayed_payment = st.slider("Number of Delayed Payments", 0, 20, 1)
+outstanding_debt = st.slider("Outstanding Debt", 0, 5000, 1)
+credit_utilization_ratio = st.slider("Credit Utilization Ratio", 20, 50, 1)
+credit_history_age = st.slider("Credit History Age", 10, 35, 1)
+
+input = {
+    'Delay_from_due_date': delay_from_due_date,
+    'Num_of_Delayed_Payment': num_of_delayed_payment,
+    'Outstanding_Debt': outstanding_debt,
+    'Credit_Utilization_Ratio': credit_utilization_ratio,
+    'Credit_History_Age': credit_history_age,
+}
+
+df = pd.DataFrame([input])
+logicPred = logistic.predict(df)
+svmLinearPred = svmLinear.predict(df)
+svmRBFPred = svmRBF.predict(df)
+
+st.write(f"Logistic Predicted Credit Score: {logicPred[0]}")
+st.write(f"Linear SVM Predicted Credit Score: {svmLinearPred[0]}")
+st.write(f"Non-Linear SVM Predicted Credit Score: {svmRBFPred[0]}")
---
+++ logistic.py
@@ -0,0 +1,36 @@
+# Exploratory data analysis
+import warnings
+warnings.filterwarnings("ignore")
+import seaborn as sns
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+st.set_option('deprecation.showPyplotGlobalUse', False)
+sns.set(color_codes=True)
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, mean_squared_error
+from sklearn.linear_model import LogisticRegression
+
+def report(data):
+    X = data.drop(columns=['Credit_Score'])
+    y = data['Credit_Score']
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Create a multiclass logistic regression model
+    logreg = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+
+    # Fit the model on the training data
+    logreg.fit(X_train, y_train)
+
+    # Make predictions on the test data
+    predictions = logreg.predict(X_test)
+
+    # Calculate Mean Squared Error (MSE)
+    mse = mean_squared_error(y_test, predictions)
+
+    # Print MSE using Streamlit
+    st.write("MSE: ", mse)
+    
+    return classification_report(y_test, predictions)
